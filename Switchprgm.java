b03f02fccaba421c82f490ee3d9b49661aa62d01
jangamyamuna <147622586+jangamyamuna@users.noreply.github.com>
2023-10-11 18:52:02 +0530

    Add files via upload

---
+++ Switchprgm.java
@@ -0,0 +1,35 @@
+package packageA1;
+
+public class Switchprgm 
+{
+	public static void main(String[] args)
+	{
+		int score = 60;
+		String grade=null;
+		switch(score/10)
+		{
+			//case 10:
+			case 9:
+				grade="A";
+				break;
+			case 8:
+				grade="B";
+				break;
+			case 7:
+				grade="C";
+				break;
+			case 6:
+				grade="D";
+				break;
+			case 5:
+				grade="E";
+				break;
+			default:
+				grade="F";
+				break;
+		}
+		System.out.println("grade:"+grade);
+		
+	}	
+		
+}
